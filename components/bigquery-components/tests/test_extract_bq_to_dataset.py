6d9c43fce26fb6c9d4bf0645d526ee7a4bd6aa22
ariadnafer <ariadna.fernandez@datatonic.com>
2023-05-23 11:16:11 +0200

    refactor: change patch formatting in all tests

--- components/bigquery-components/tests/test_extract_bq_to_dataset.py
+++ components/bigquery-components/tests/test_extract_bq_to_dataset.py
@@ -7,67 +7,58 @@ import bigquery_components
 extract_bq_to_dataset = bigquery_components.extract_bq_to_dataset.python_func
 
 
-def test_extract_bq_to_dataset(tmpdir):
-    with mock.patch("google.cloud.bigquery.client.Client") as mock_client, mock.patch(
-        "google.cloud.bigquery.job.ExtractJobConfig"
-    ) as mock_job_config, mock.patch("google.cloud.bigquery.table.Table") as mock_table:
+@mock.patch("google.cloud.bigquery.client.Client")
+@mock.patch("google.cloud.bigquery.table.Table")
+@mock.patch("google.cloud.bigquery.job.ExtractJobConfig")
+def test_extract_bq_to_dataset(mock_job_config, mock_table, mock_client, tmpdir):
+    """
+    Checks that the extract_bq_to_dataset is called correctly
+    """
+    mock_path = tmpdir
+    mock_client.extract_table.return_value = "my-job"
+    mock_table.return_value.table_ref = "my-table"
+    mock_job_config.return_value = "mock-job-config"
 
-        # Mock the Dataset path
-        mock_path = tmpdir
+    extract_bq_to_dataset(
+        bq_client_project_id="my-project-id",
+        source_project_id="source-project-id",
+        dataset_id="dataset-id",
+        table_name="table-name",
+        dataset=Dataset(uri=mock_path),
+        destination_gcs_uri="gs://mock_bucket",
+        dataset_location="EU",
+        extract_job_config=None,
+        skip_if_exists=False,
+    )
 
-        # Set up the mock Client
-        mock_client.extract_table.return_value = "my-job"
-
-        # Set up the mock Table
-        mock_table.return_value.table_ref = "my-table"
-
-        # Set up the mock ExtractJob
-        mock_job_config.return_value = "mock-job-config"
-
-        # Call the function
-        extract_bq_to_dataset(
-            bq_client_project_id="my-project-id",
-            source_project_id="source-project-id",
-            dataset_id="dataset-id",
-            table_name="table-name",
-            dataset=Dataset(uri=mock_path),
-            destination_gcs_uri="gs://mock_bucket",
-            dataset_location="EU",
-            extract_job_config=None,
-            skip_if_exists=False,
-        )
-
-        # Check that client.extract_table was called correctly
-        mock_client.return_value.extract_table.assert_called_once_with(
-            mock_table.return_value, "gs://mock_bucket", job_config="mock-job-config"
-        )
+    mock_client.return_value.extract_table.assert_called_once_with(
+        mock_table.return_value, "gs://mock_bucket", job_config="mock-job-config"
+    )
 
 
-def test_extract_bq_to_dataset_skip_existing(tmpdir):
-    with mock.patch("google.cloud.bigquery.client.Client") as mock_client, mock.patch(
-        "google.cloud.bigquery.table.Table"
-    ), mock.patch("google.cloud.bigquery.job.ExtractJobConfig"), mock.patch(
-        "pathlib.Path.exists"
-    ) as mock_path_exists:
+@mock.patch("google.cloud.bigquery.client.Client")
+@mock.patch("google.cloud.bigquery.table.Table")
+@mock.patch("google.cloud.bigquery.job.ExtractJobConfig")
+@mock.patch("pathlib.Path.exists")
+def test_extract_bq_to_dataset_skip_existing(
+    mock_path_exists, mock_job_config, mock_table, mock_client, tmpdir
+):
+    """
+    Checks that when the dataset exists the method is not called
+    """
+    mock_path = tmpdir
+    mock_path_exists.return_value = True
 
-        # Mock the Dataset path
-        mock_path = tmpdir
+    extract_bq_to_dataset(
+        bq_client_project_id="my-project-id",
+        source_project_id="source-project-id",
+        dataset_id="dataset-id",
+        table_name="table-name",
+        dataset=Dataset(uri=mock_path),
+        destination_gcs_uri="gs://mock_bucket",
+        dataset_location="EU",
+        extract_job_config=None,
+        skip_if_exists=True,
+    )
 
-        # Mock that the destination already exists
-        mock_path_exists.return_value = True
-
-        # Call the function
-        extract_bq_to_dataset(
-            bq_client_project_id="my-project-id",
-            source_project_id="source-project-id",
-            dataset_id="dataset-id",
-            table_name="table-name",
-            dataset=Dataset(uri=mock_path),
-            destination_gcs_uri="gs://mock_bucket",
-            dataset_location="EU",
-            extract_job_config=None,
-            skip_if_exists=True,
-        )
-
-        # Ensure that Client.extract_table was not called
-        assert not mock_client.return_value.extract_table.called
+    assert not mock_client.return_value.extract_table.called
--- components/vertex-components/tests/test_custom_training_job.py
+++ components/vertex-components/tests/test_custom_training_job.py
@@ -9,25 +9,69 @@ import vertex_components
 custom_train_job = vertex_components.custom_train_job.python_func
 
 
-def test_custom_train_job(tmpdir):
-    with mock.patch(
-        "google.cloud.aiplatform.CustomTrainingJob"
-    ) as mock_job, mock.patch("os.path.exists") as mock_exists, mock.patch(
-        "builtins.open", mock.mock_open(read_data="{}")
-    ) as mock_open:
+@mock.patch("google.cloud.aiplatform.CustomTrainingJob")
+@mock.patch("os.path.exists")
+@mock.patch("builtins.open", new_callable=mock.mock_open, read_data="{}")
+def test_custom_train_job(mock_open, mock_exists, mock_job, tmpdir):
+    """
+    Checks that the custom job method is called
+    """
+    mock_exists.return_value = True
 
-        # Mock that the training script exists
-        mock_exists.return_value = True
+    mock_train_data = Dataset(uri=tmpdir)
+    mock_valid_data = Dataset(uri=tmpdir)
+    mock_test_data = Dataset(uri=tmpdir)
+    mock_model = Artifact(uri=tmpdir, metadata={"resourceName": ""})
+    mock_metrics = Metrics(uri=tmpdir)
 
-        # Mock Artifacts
-        mock_train_data = Dataset(uri=tmpdir)
-        mock_valid_data = Dataset(uri=tmpdir)
-        mock_test_data = Dataset(uri=tmpdir)
+    custom_train_job(
+        train_script_uri="gs://my-bucket/train_script.py",
+        train_data=mock_train_data,
+        valid_data=mock_valid_data,
+        test_data=mock_test_data,
+        project_id="my-project-id",
+        project_location="europe-west4",
+        model_display_name="my-model",
+        train_container_uri="gcr.io/my-project/my-image:latest",
+        serving_container_uri="gcr.io/my-project/my-serving-image:latest",
+        model=mock_model,
+        metrics=mock_metrics,
+        staging_bucket="gs://my-bucket",
+        job_name="my-job",
+    )
 
-        mock_model = Artifact(uri=tmpdir, metadata={"resourceName": ""})
-        mock_metrics = Metrics(uri=tmpdir)
+    mock_job.assert_called_once_with(
+        project="my-project-id",
+        location="europe-west4",
+        staging_bucket="gs://my-bucket",
+        display_name="my-job",
+        script_path="/gcs/my-bucket/train_script.py",
+        container_uri="gcr.io/my-project/my-image:latest",
+        requirements=None,
+        model_serving_container_image_uri="gcr.io/my-project/my-serving-image:latest",  # noqa: E501
+    )
 
-        # Call function
+    # Assert metrics loading
+    mock_open.assert_called_once_with(tmpdir, "r")
+
+
+@mock.patch("google.cloud.aiplatform.CustomTrainingJob")
+@mock.patch("os.path.exists")
+@mock.patch("builtins.open", new_callable=mock.mock_open, read_data="{}")
+def test_custom_train_script_not_found(mock_open, mock_exists, mock_job, tmpdir):
+    """
+    Checks that when the training script is not found
+    the method fails
+    """
+    mock_exists.return_value = False
+
+    mock_train_data = Dataset(uri=tmpdir)
+    mock_valid_data = Dataset(uri=tmpdir)
+    mock_test_data = Dataset(uri=tmpdir)
+    mock_model = Artifact(uri=tmpdir, metadata={"resourceName": ""})
+    mock_metrics = Metrics(uri=tmpdir)
+
+    with pytest.raises(ValueError):
         custom_train_job(
             train_script_uri="gs://my-bucket/train_script.py",
             train_data=mock_train_data,
@@ -44,55 +88,6 @@ def test_custom_train_job(tmpdir):
             job_name="my-job",
         )
 
-        # Assert custom training job is called
-        mock_job.assert_called_once_with(
-            project="my-project-id",
-            location="europe-west4",
-            staging_bucket="gs://my-bucket",
-            display_name="my-job",
-            script_path="/gcs/my-bucket/train_script.py",
-            container_uri="gcr.io/my-project/my-image:latest",
-            requirements=None,
-            model_serving_container_image_uri="gcr.io/my-project/my-serving-image:latest",  # noqa: E501
-        )
-
-        # Assert metrics loading
-        mock_open.assert_called_once_with(tmpdir, "r")
-
-
-def test_custom_train_script_not_found(tmpdir):
-    with pytest.raises(ValueError), mock.patch(
-        "google.cloud.aiplatform.CustomTrainingJob"
-    ) as mock_job, mock.patch("os.path.exists") as mock_exists, mock.patch(
-        "builtins.open", mock.mock_open(read_data="{}")
-    ) as mock_open:
-
-        # Mock that the training script is not found
-        mock_exists.return_value = False
-
-        mock_train_data = Dataset(uri=tmpdir)
-        mock_valid_data = Dataset(uri=tmpdir)
-        mock_test_data = Dataset(uri=tmpdir)
-
-        mock_model = Artifact(uri=tmpdir, metadata={"resourceName": ""})
-        mock_metrics = Metrics(uri=tmpdir)
-
-        custom_train_job(
-            train_script_uri="gs://my-bucket/train_script.py",
-            train_data=mock_train_data,
-            valid_data=mock_valid_data,
-            test_data=mock_test_data,
-            project_id="my-project-id",
-            project_location="europe-west4",
-            model_display_name="my-model",
-            train_container_uri="gcr.io/my-project/my-image:latest",
-            serving_container_uri="gcr.io/my-project/my-serving-image:latest",
-            model=mock_model,
-            metrics=mock_metrics,
-            staging_bucket="gs://my-bucket",
-            job_name="my-job",
-        )
-
-        # Assert the custom training job is not executed
-        mock_job.assert_not_called()
-        mock_open.assert_not_called()
+    # Assert the custom training job is not executed
+    mock_job.assert_not_called()
+    mock_open.assert_not_called()
