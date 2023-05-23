e8f2b2cab380396fb15478ca7f71232398b20206
lakshmimanasa37 <35919173+lakshmimanasa37@users.noreply.github.com>
2023-05-23 09:58:42 +0100

    Update ingest.sql

--- pipelines/pipelines/xgboost/training/queries/ingest.sql
+++ pipelines/pipelines/xgboost/training/queries/ingest.sql
@@ -28,30 +28,14 @@ with filter_start_values as (
          DATE_SUB(DATE(CAST(filter_start_values.filter_start_value as DATETIME)), INTERVAL 3 MONTH) AND
          DATE_SUB(DATE(filter_start_value), INTERVAL 2 MONTH)
 )
--- Use the average trip_seconds as a replacement for NULL or 0 values
-,mean_time as (
-    SELECT CAST(avg(trip_seconds) AS INT64) as avg_trip_seconds
-    FROM filtered_data
-)
+
 
 SELECT
-    CAST(EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS FLOAT64) AS dayofweek,
-    CAST(EXTRACT(HOUR FROM trip_start_timestamp) AS FLOAT64) AS hourofday,
-    ST_DISTANCE(
-        ST_GEOGPOINT(pickup_longitude, pickup_latitude),
-        ST_GEOGPOINT(dropoff_longitude, dropoff_latitude)) AS trip_distance,
-    trip_miles,
-    CAST( CASE WHEN trip_seconds is NULL then m.avg_trip_seconds
-               WHEN trip_seconds <= 0 then m.avg_trip_seconds
-               ELSE trip_seconds 
-               END AS FLOAT64) AS trip_seconds,
-    payment_type,
-    company,
-    (fare + tips + tolls + extras) AS `{{ target_column }}`,
-FROM filtered_data as t, mean_time as m
+    start_station_name,
+    starttime,
+    end_station_name,
+    IF(EXTRACT(DAYOFWEEK FROM starttime) = 1 OR EXTRACT(DAYOFWEEK FROM starttime) = 7, true, false) AS is_weekend,
+    tripduration AS `{{ target_column }}`
+FROM `bigquery-public-data.new_york_citibike.citibike_trips`
 WHERE
-    trip_miles > 0 AND fare > 0 AND fare < 1500
-    {% for field in ["fare", "trip_start_timestamp", "pickup_longitude",
-                "pickup_latitude", "dropoff_longitude", "dropoff_latitude","payment_type","company"] %}
-        AND `{{ field }}` IS NOT NULL
-    {% endfor %}
+    (tripduration IS NOT NULL OR tripduration > 0)
