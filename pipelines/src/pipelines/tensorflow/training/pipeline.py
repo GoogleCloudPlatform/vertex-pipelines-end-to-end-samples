ece44440fd4f1306c3e0dfd03a48af03ec912882
jan-zajac-dt <jan.zajac@datatonic.com>
2023-06-05 10:06:34 +0100

    Add ADDITIONAL_SUFFIX for all other pipelines

--- pipelines/src/pipelines/tensorflow/training/pipeline.py
+++ pipelines/src/pipelines/tensorflow/training/pipeline.py
@@ -39,6 +39,7 @@ def tensorflow_pipeline(
     timestamp: str = "2022-12-01 00:00:00",
     staging_bucket: str = os.environ.get("VERTEX_PIPELINE_ROOT"),
     pipeline_files_gcs_path: str = os.environ.get("PIPELINE_FILES_GCS_PATH"),
+    additional_suffix: str = os.environ.get("ADDITIONAL_SUFFIX"),
     test_dataset_uri: str = "",
 ):
     """
@@ -65,6 +66,8 @@ def tensorflow_pipeline(
             If any time part is missing, it will be regarded as zero.
         staging_bucket (str): Staging bucket for pipeline artifacts.
         pipeline_files_gcs_path (str): GCS path where the pipeline files are located
+        additional_suffix (str): Optional. Additional suffix to append GCS asset path
+            and BQ tables that get overwritten.
         test_dataset_uri (str): Optional. GCS URI of statis held-out test dataset.
     """
 
@@ -73,14 +76,16 @@ def tensorflow_pipeline(
     label_column_name = "total_fare"
     time_column = "trip_start_timestamp"
     ingestion_table = "taxi_trips"
-    table_suffix = "_tf_training"  # suffix to table names
+    table_suffix = "_tf_training" + str(additional_suffix)  # suffix to table names
     ingested_table = "ingested_data" + table_suffix
     preprocessed_table = "preprocessed_data" + table_suffix
     train_table = "train_data" + table_suffix
     valid_table = "valid_data" + table_suffix
     test_table = "test_data" + table_suffix
     primary_metric = "rootMeanSquaredError"
-    train_script_uri = f"{pipeline_files_gcs_path}/assets/train_tf_model.py"
+    train_script_uri = (
+        f"{pipeline_files_gcs_path}/assets{additional_suffix}/train_tf_model.py"
+    )
     hparams = dict(
         batch_size=100,
         epochs=5,
