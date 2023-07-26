5abfa36b14645275fb782dd54d88851ebf6d73c6
Jonny Browning <jonathan.browning@datatonic.com>
2023-07-26 13:17:34 +0100

    fix: restore lookup_model component for prediction pipeline

--- components/vertex-components/src/vertex_components/__init__.py
+++ components/vertex-components/src/vertex_components/__init__.py
@@ -1,9 +1,11 @@
+from .lookup_model import lookup_model
 from .model_batch_predict import model_batch_predict
 from .upload_model import upload_model
 
 
 __version__ = "0.0.1"
 __all__ = [
+    "lookup_model",
     "model_batch_predict",
     "upload_model",
 ]
